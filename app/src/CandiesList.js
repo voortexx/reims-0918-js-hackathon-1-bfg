41e5decbba5f36e5b5ae4c97e369af116f16eaa8
Simonin Gautier <gautier08000@hotmail.com>
2018-10-30 17:39:26 +0100

    style css button

--- app/src/CandiesList.js
+++ app/src/CandiesList.js
@@ -4,8 +4,8 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
 const CandiesList = ({ candies }) => {
   return (
     <div>
-      <Card>
-        <CardImg top width="30%" src={candies.image_url} alt="Card image cap" />
+      <Card className="shadow">
+        <CardImg src={candies.image_url} alt="Card image cap" />
         <CardBody>
           <CardTitle>{candies.product_name}</CardTitle>
           <CardText>{candies.brands}</CardText>
--- app/src/HomeButtons.js
+++ app/src/HomeButtons.js
@@ -4,9 +4,43 @@ import { Button, Row } from "reactstrap";
 const HomeButtons = () => {
   return (
     <Row>
-      <Button color="primary">Adresses</Button>
+      <Button color="primary">
+        <div id="word">
+          <div id="glitches">
+            <span id="main">Adresses</span>
+            <div id="letters">
+              <span class="letter">A</span>
+              <span class="letter">d</span>
+              <span class="letter">r</span>
+              <span class="letter">e</span>
+              <span class="letter">s</span>
+              <span class="letter">s</span>
+              <span class="letter">e</span>
+              <span class="letter">s</span>
+            </div>
+          </div>
+        </div>
+      </Button>
 
-      <Button color="primary">Inventaire</Button>
+      <Button color="primary">
+        {" "}
+        <div id="word">
+          <div id="glitches">
+            <span id="main">Inventory</span>
+            <div id="letters">
+              <span class="letter">I</span>
+              <span class="letter">n</span>
+              <span class="letter">v</span>
+              <span class="letter">e</span>
+              <span class="letter">n</span>
+              <span class="letter">t</span>
+              <span class="letter">o</span>
+              <span class="letter">r</span>
+              <span class="letter">y</span>
+            </div>
+          </div>
+        </div>
+      </Button>
     </Row>
   );
 };
