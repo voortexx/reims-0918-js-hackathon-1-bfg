c05b45aca26f4a47c6653e99544d2c50af1260d3
Fabien Raymond <voortexxx@gmail.com>
2018-11-09 19:22:17 +0100

    change title

---
+++ build/precache-manifest.b12db4b76388b79b41aa14c0b7f2ca09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e51ecc3c103f3b70aeef5a170a2ddb11",
+    "url": "/reims-0918-js-hackathon-1-bfg/static/media/witch.e51ecc3c.gif"
+  },
+  {
+    "revision": "561196e5cf6a47640bfb80d6be434374",
+    "url": "/reims-0918-js-hackathon-1-bfg/static/media/toile.561196e5.png"
+  },
+  {
+    "revision": "064ffee8cc2de0889ec9592593ac27a1",
+    "url": "/reims-0918-js-hackathon-1-bfg/static/media/bg.064ffee8.jpg"
+  },
+  {
+    "revision": "50c28c4d72dc005619c5",
+    "url": "/reims-0918-js-hackathon-1-bfg/static/js/runtime~main.50c28c4d.js"
+  },
+  {
+    "revision": "8901740b2fa6b35b447a",
+    "url": "/reims-0918-js-hackathon-1-bfg/static/js/main.8901740b.chunk.js"
+  },
+  {
+    "revision": "83726b00438bb1e28303",
+    "url": "/reims-0918-js-hackathon-1-bfg/static/js/1.83726b00.chunk.js"
+  },
+  {
+    "revision": "8901740b2fa6b35b447a",
+    "url": "/reims-0918-js-hackathon-1-bfg/static/css/main.b1ad1a29.chunk.css"
+  },
+  {
+    "revision": "83726b00438bb1e28303",
+    "url": "/reims-0918-js-hackathon-1-bfg/static/css/1.8f41421b.chunk.css"
+  },
+  {
+    "revision": "a1a1ebf5c2e3b9e22e7140da84364622",
+    "url": "/reims-0918-js-hackathon-1-bfg/index.html"
+  }
+];
